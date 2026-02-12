b33ce4970f064bddf432736cf1fac6e07f67dfa4
KyawZaw-112 <kyawzawwin.2001.cs@gmail.com>
2026-02-12 11:32:18 +0700

    fix: update route params type for latest next.js

--- app/api/admin/admin/subscription/extend/[id]/route.ts
+++ app/api/admin/admin/subscription/extend/[id]/route.ts
@@ -8,10 +8,10 @@ const supabase = createClient(
 
 export async function POST(
     req: NextRequest,
-    { params }: { params: { id: string } }
+    { params }: { params:Promise< { id: string }> }
 ) {
     try {
-        const { id } = params;
+        const { id } = await params;
 
         const { data: sub, error } = await supabase
             .from("subscriptions")
