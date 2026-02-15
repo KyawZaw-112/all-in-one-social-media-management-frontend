ef6bba477ef9d9469ae30736002c59dbfab28909
KyawZaw-112 <kyawzawwin.2001.cs@gmail.com>
2026-02-16 00:28:02 +0700

    fixed:move oauth action move to backend

--- app/dashboard/platforms/page.tsx
+++ app/dashboard/platforms/page.tsx
@@ -1,31 +1,13 @@
 "use client";
 
 import {Button} from "antd";
-import supabase from "@/lib/supabase";
 
 export default function PlatformsPage() {
 
-    const connectFacebook = async () => {
-        const { data } = await supabase.auth.getSession();
-        const session = data.session;
-
-        if (!session) {
-            alert("Not authenticated");
-            return;
-        }
-
-        // const facebookAppId = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID;
-        // const redirectUri = "https://all-in-one-social-media-management.onrender.com/api/oauth/facebook/callback";
-
-        const url =
-            `https://www.facebook.com/v19.0/dialog/oauth?` +
-            `client_id=789934663517168` +
-            `&redirect_uri=https://all-in-one-social-media-management.onrender.com/api/oauth/facebook/callback` +
-            `&response_type=code` +
-            `&scope=email`;
-
-        window.location.href = url;
+    const connectFacebook = () => {
+        window.location.href = "http://localhost:4000/api/oauth/facebook";
     };
+
 
 
     return (
