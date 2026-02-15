a4fe988bea1f45ae4035c43367153522d85a9433
KyawZaw-112 <kyawzawwin.2001.cs@gmail.com>
2026-02-15 23:57:52 +0700

    fix:facebook

--- app/dashboard/platforms/page.tsx
+++ app/dashboard/platforms/page.tsx
@@ -1,26 +1,35 @@
 "use client";
 
-import { Button } from "antd";
+import {Button} from "antd";
 import supabase from "@/lib/supabase";
 
 export default function PlatformsPage() {
 
     const connectFacebook = async () => {
         const { data } = await supabase.auth.getSession();
-        const token = data.session?.access_token;
+        const session = data.session;
 
-        if (!token) {
+        if (!session) {
             alert("Not authenticated");
             return;
         }
 
-        // Redirect directly (NO fetch)
-        window.location.href =
-            `http://localhost:4000/api/oauth/facebook?token=${token}`;
+        // const facebookAppId = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID;
+        // const redirectUri = "https://all-in-one-social-media-management.onrender.com/api/oauth/facebook/callback";
+
+        const url =
+            `https://www.facebook.com/v19.0/dialog/oauth?` +
+            `client_id=789934663517168` +
+            `&redirect_uri=https://all-in-one-social-media-management.onrender.com/api/oauth/facebook/callback` +
+            `&response_type=code` +
+            `&scope=email`;
+
+        window.location.href = url;
     };
 
+
     return (
-        <div style={{ padding: 40 }}>
+        <div style={{padding: 40}}>
             <h2>Connect Facebook</h2>
             <Button type="primary" onClick={connectFacebook}>
                 Connect Facebook
