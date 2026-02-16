a2f79ffb2feb646624b64521a818bb0b3116f4eb
KyawZaw-112 <kyawzawwin.2001.cs@gmail.com>
2026-02-17 05:09:14 +0700

    fixed: platform undefined

--- app/dashboard/pages/page.tsx
+++ app/dashboard/pages/page.tsx
@@ -69,6 +69,8 @@ export default function PagesListPage() {
 
     const router = useRouter();
 
+    console.log(router)
+
     return (
         <div className="p-6">
             <h1 className="text-2xl font-semibold mb-6">Connected Pages</h1>
